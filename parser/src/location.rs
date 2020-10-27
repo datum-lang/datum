e76a07236ab3e47f0ccbf914d41b28b95fa9bd55
Phodal Huang <h@phodal.com>
2020-10-27 20:24:30 +0800

    feat: import location from RustPython for redesign

--- parser/src/location.rs
+++ parser/src/location.rs
@@ -6,3 +6,54 @@ impl Loc {
         Loc(file_no, start, end)
     }
 }
+
+//! Datatypes to support source location information.
+use std::fmt;
+
+/// A location somewhere in the sourcecode.
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+pub struct Location {
+    row: usize,
+    column: usize,
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "line {} column {}", self.row, self.column)
+    }
+}
+
+impl Location {
+    pub fn visualize(&self, line: &str, desc: &str) -> String {
+        // desc.to_owned()
+        format!("{}\n{}\n{}↑", desc, line, " ".repeat(self.column - 1))
+    }
+}
+
+impl Location {
+    pub fn new(row: usize, column: usize) -> Self {
+        Location { row, column }
+    }
+
+    pub fn row(&self) -> usize {
+        self.row
+    }
+
+    pub fn column(&self) -> usize {
+        self.column
+    }
+
+    pub fn reset(&mut self) {
+        self.row = 1;
+        self.column = 1;
+    }
+
+    pub fn go_right(&mut self) {
+        self.column += 1;
+    }
+
+    pub fn newline(&mut self) {
+        self.row += 1;
+        self.column = 1;
+    }
+}
