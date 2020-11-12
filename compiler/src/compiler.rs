cb0aaf4166d579e8c957a14d8336ad7585f2b742
Phodal Huang <h@phodal.com>
2020-11-12 13:21:38 +0800

    test: try to make first tests

--- compiler/src/compiler.rs
+++ compiler/src/compiler.rs
@@ -313,7 +313,7 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
         }
     }
 
-    pub fn create(input: &str) {
+    pub fn create(input: &str) -> Result<String, ()> {
         let context = Context::create();
         let module = context.create_module("repl");
         let builder = context.create_builder();
@@ -336,8 +336,9 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
                 let compiler = Compiler::compile(&context, &builder, &fpm, &module, &unit);
                 let _r = compiler.dump_llvm("demo.ll".as_ref());
                 compiler.run_jit();
+                Ok(compiler.module.print_to_string().to_string())
             }
-            Err(_) => {}
+            Err(_) => Err(()),
         }
     }
 }
@@ -349,6 +350,8 @@ mod test {
     #[test]
     #[rustfmt::skip]
     fn init_parser() {
-        Compiler::create("default$main(string name) {fmt.println(\"hello,world\")}");
+        let result = Compiler::create("default$main() {fmt.println(\"hello,world\")}");
+        assert_eq!("; ModuleID = \'repl\'
+source_filename = \"repl\"\ntarget datalayout = \"e-m:o-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128\"\n\n@hello = internal global [14 x i8] c\"hello, world!\\0A\"\n\ndefine i32 @main() {\nmain:\n  %0 = call i32 (i8*, ...) @puts(i8* getelementptr inbounds ([14 x i8], [14 x i8]* @hello, i32 0, i32 0))\n  ret i32 0\n}\n\ndeclare i32 @puts(i8*, ...)\n\n; Function Attrs: nounwind\ndeclare void @llvm.stackprotector(i8*, i8**) #0\n\nattributes #0 = { nounwind }\n", result.unwrap());
     }
 }
