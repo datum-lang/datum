353f186ad3bd7a2588c3da1838dfe474eb07591a
Phodal Huang <h@phodal.com>
2020-11-18 13:01:09 +0800

    feat: init namespaces fields

--- compiler/src/namespace.rs
+++ compiler/src/namespace.rs
@@ -1,11 +1,35 @@
+#[derive(Clone, Debug)]
+// #[allow(clippy::large_enum_variant)]
+pub enum Statement {
+    VariableDecl()
+}
+
 #[derive(PartialEq, Clone, Debug)]
-pub struct StructFuncDecl {
+pub enum Expression {
+
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Parameter {
+    pub name: String,
+}
+
+pub struct Function {
+    pub name: String,
+    pub params: Vec<Parameter>,
+    pub returns: Vec<Parameter>,
+    pub body: Vec<Statement>,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Struct {
     pub name: String,
 }
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct StructDecl {
     pub name: String,
+    pub functions: Vec<Function>,
 }
 
 pub struct Namespace {
