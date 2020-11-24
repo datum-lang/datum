2377695c774a2324ab21821663f5865ff172c32f
Phodal Huang <h@phodal.com>
2020-11-24 23:08:33 +0800

    refactor: move symbol table for neat

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -3,4 +3,3 @@ pub use neat::*;
 pub mod lowerify;
 pub mod medium;
 pub mod neat;
-pub mod symbol_table;
--- compiler/src/neat/mod.rs
+++ compiler/src/neat/mod.rs
@@ -3,6 +3,7 @@ pub use namespace::*;
 pub use program::*;
 pub use statements::*;
 pub use struct_function::*;
+pub use symbol_table::*;
 pub use unit::*;
 
 pub mod builtin;
@@ -11,6 +12,7 @@ pub mod namespace;
 pub mod program;
 pub mod statements;
 pub mod struct_function;
+pub mod symbol_table;
 pub mod unit;
 
 #[cfg(test)]
