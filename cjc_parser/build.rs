c4c3cc530b0d959071f52d16d0f23cdb7c0a2bba
Phodal Huang <h@phodal.com>
2020-12-20 00:46:09 +0800

    bulid: add build scripts for local source

--- cjc_parser/build.rs
+++ cjc_parser/build.rs
@@ -2,4 +2,11 @@ extern crate lalrpop;
 
 fn main() {
     lalrpop::process_root().unwrap();
+
+    // todo: thinking in generate code in codebase
+    // lalrpop::Configuration::new()
+    //     .generate_in_source_tree()
+    //     .emit_rerun_directives(true)
+    //     .process()
+    //     .unwrap();
 }
