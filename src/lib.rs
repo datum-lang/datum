d7d66732ed176017d3e4c9c08ec3922a3a1ff319
Phodal Huang <h@phodal.com>
2020-12-25 16:57:36 +0800

    test: add call demos

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,14 @@ mod test {
 
         cmd.assert().success().stdout("hello,world\n");
     }
+
+    #[test]
+    fn should_run_call_hello_world_file() {
+        use assert_cmd::Command;
+
+        let mut cmd = Command::cargo_bin("cjc").unwrap();
+        cmd.arg("docs/examples/func-call.cj").unwrap();
+
+        cmd.assert().success().stdout("你好，世界！\n");
+    }
 }
