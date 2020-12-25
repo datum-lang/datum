af8f9205bd85b99865579ab2e0c732b6c637708c
Phodal Huang <h@phodal.com>
2020-12-25 16:32:52 +0800

    refactor: make hello world file runable

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+#[cfg(test)]
+mod test {
+    #[test]
+    fn should_run_hello_world_file() {
+        use assert_cmd::Command;
 
+        let mut cmd = Command::cargo_bin("cjc").unwrap();
+        cmd.arg("docs/examples/hello-world.cj").unwrap();
+
+        cmd.assert().success().stdout("hello,world\n");
+    }
+}
