8c81006932127197a73568fae64f13e3b3692d03
Phodal Huang <h@phodal.com>
2021-01-07 09:13:10 +0800

    feat(lsp): impl more lsp interface for testing

--- src/bin/languageserver/mod.rs
+++ src/bin/languageserver/mod.rs
@@ -1,10 +1,54 @@
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::sync::Mutex;
+
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+use serde_json::Value;
+
+#[derive(Debug)]
+pub struct FileOffsets {
+    files: Vec<Vec<usize>>,
+}
+
+#[derive(Debug)]
+pub struct Hovers {
+    offsets: FileOffsets,
+    lookup: Vec<(usize, usize, String)>,
+}
+
 #[derive(Debug)]
 struct CharjServer {
     client: Client,
+    files: Mutex<HashMap<PathBuf, Hovers>>,
+}
+
+pub fn start_server() {
+    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        let stdin = tokio::io::stdin();
+        let stdout = tokio::io::stdout();
+
+        let (service, messages) = LspService::new(|client| CharjServer {
+            client,
+            files: Mutex::new(HashMap::new()),
+        });
+
+        Server::new(stdin, stdout)
+            .interleave(messages)
+            .serve(service)
+            .await;
+    });
+    std::process::exit(1);
+}
+
+impl CharjServer {
+    /// Parse file
+    async fn parse_file(&self, uri: Url) {
+        if let Ok(path) = uri.to_file_path() {}
+    }
 }
 
 #[tower_lsp::async_trait]
@@ -22,20 +66,61 @@ impl LanguageServer for CharjServer {
     async fn shutdown(&self) -> Result<()> {
         Ok(())
     }
-}
 
-pub fn start_server() {
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(async {
-        let stdin = tokio::io::stdin();
-        let stdout = tokio::io::stdout();
-
-        let (service, messages) = LspService::new(|client| CharjServer { client });
-
-        Server::new(stdin, stdout)
-            .interleave(messages)
-            .serve(service)
+    async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
+        self.client
+            .log_message(MessageType::Info, "workspace folders changed!")
             .await;
-    });
-    std::process::exit(1);
+    }
+
+    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+        self.client
+            .log_message(MessageType::Info, "configuration changed!")
+            .await;
+    }
+
+    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+        self.client
+            .log_message(MessageType::Info, "watched files have changed!")
+            .await;
+    }
+
+    async fn execute_command(&self, _: ExecuteCommandParams) -> Result<Option<Value>> {
+        self.client
+            .log_message(MessageType::Info, "command executed!")
+            .await;
+        Ok(None)
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        let uri = params.text_document.uri;
+
+        self.parse_file(uri).await;
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        let uri = params.text_document.uri;
+
+        self.parse_file(uri).await;
+    }
+
+    async fn did_save(&self, params: DidSaveTextDocumentParams) {
+        let uri = params.text_document.uri;
+
+        self.parse_file(uri).await;
+    }
+
+    async fn did_close(&self, params: DidCloseTextDocumentParams) {
+        let uri = params.text_document.uri;
+
+        if let Ok(path) = uri.to_file_path() {
+            if let Ok(mut files) = self.files.lock() {
+                files.remove(&path);
+            }
+        }
+    }
+
+    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+        Ok(None)
+    }
 }
