dbc3bf5e1e280aa864cd5b66346477a0904a8785
Phodal Huang <h@phodal.com>
2021-01-12 10:34:43 +0800

    refactor: rename cli to dc

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod test {
     fn should_run_hello_world_file() {
         use assert_cmd::Command;
 
-        let mut cmd = Command::cargo_bin("cjc").unwrap();
+        let mut cmd = Command::cargo_bin("dc").unwrap();
         cmd.arg("docs/examples/hello-world.cj").unwrap();
 
         cmd.assert().success().stdout("hello,world\n");
@@ -14,7 +14,7 @@ mod test {
     fn should_run_call_hello_world_file() {
         use assert_cmd::Command;
 
-        let mut cmd = Command::cargo_bin("cjc").unwrap();
+        let mut cmd = Command::cargo_bin("dc").unwrap();
         cmd.arg("docs/examples/func-call.cj").unwrap();
 
         cmd.assert().success().stdout("你好，世界！\n");
